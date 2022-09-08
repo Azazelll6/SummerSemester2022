78e02346aa05e8f61f235f36a9d1880d67d44a92
Zac Peterson <Peterson.Zac17@gmail.com>
2022-09-07 20:58:32 -0600

    Finished UnityEvents lab

--- MatchingGame/Assets/Scripts/MonoEventsBehavior.cs
+++ MatchingGame/Assets/Scripts/MonoEventsBehavior.cs
@@ -1,21 +1,24 @@
-
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MonoEventsBehavior : MonoBehaviour
 {
     public UnityEvent startEvent, awakeEvent, disableEvent;
-    // Start is called before the first frame update
+    
+    
     private void Awake()
     {
         awakeEvent.Invoke();
     }
-    
+
     private void Start()
     {
         startEvent.Invoke();
     }
-    
+
     private void OnDisable()
     {
         disableEvent.Invoke();
     }
+
+}
