135f1fef9c2b4c9a3414dbceb5af509776ee4337
Zac <10719862@uvu.edu>
2022-09-12 18:20:48 -0600

    Added Draggable Behavior

---
+++ MatchingGame/Assets/Scripts/DraggableBehavior.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableBehavior : MonoBehaviour
+{
+    private Camera cameraObj;
+    public bool draggable;
+    
+    void Start()
+    {
+        cameraObj = Camera.main;
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        draggable = true;
+
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+            Debug.Log("Drag");
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        draggable = false;
+    }
+}
