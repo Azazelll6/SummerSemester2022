2612f18fba9fff7f817b715786e61286284de1ae
Zac Peterson <Peterson.Zac17@gmail.com>
2022-09-12 19:17:01 -0600

    Dragging now works

--- MatchingGame/Assets/Scripts/DraggableBehavior.cs
+++ MatchingGame/Assets/Scripts/DraggableBehavior.cs
@@ -7,6 +7,7 @@ public class DraggableBehavior : MonoBehaviour
 {
     private Camera cameraObj;
     public bool draggable;
+    public Vector3 position, offset;
     
     void Start()
     {
@@ -15,12 +16,22 @@ public class DraggableBehavior : MonoBehaviour
 
     public IEnumerator OnMouseDown()
     {
+        //creates offset length data between where the object is in relation to the world - the length data
+        //between where the mouse is from the camera (camera is 0 0 0)
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
         draggable = true;
-
+        yield return new WaitForFixedUpdate();
+        
         while (draggable)
         {
+            //waits for a fixed update before performing next steps
             yield return new WaitForFixedUpdate();
-            Debug.Log("Drag");
+            //Gets the mouse position on drag (does not move object just supplies data which is not offset)
+            //meaning it's transform does not look right
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            //Applies the mouse position (gotten from line above & the defined offset) to the vector of the object
+            transform.position = position;
+            //Debug.Log("Drag");
         }
     }
 
