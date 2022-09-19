e9cad8a1fb9feb83462eab5e425b71d85358f990
Zac Peterson <Peterson.Zac17@gmail.com>
2022-09-18 22:24:30 -0600

    Completed InstancerGO to Vector3

--- MatchingGame/Assets/Scripts/Instancer.cs
+++ MatchingGame/Assets/Scripts/Instancer.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Instancer : ScriptableObject
 {
-    public void CreateInstance(GameObject obj)
+    public GameObject prefab;
+    
+    public void CreateInstance()
     {
-        Instantiate(obj);
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Data obj)
+    {
+        //Quaternion.Identity means that we are not going to rotate the object at all. Rotate values at 0.
+        Instantiate(prefab, obj.value, Quaternion.identity);
     }
 }
