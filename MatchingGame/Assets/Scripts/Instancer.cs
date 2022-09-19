82188a20ac80520c26e8c3b64c9bc11ab6ffa39d
Zac Peterson <Peterson.Zac17@gmail.com>
2022-09-18 22:42:49 -0600

    Completed Vector3fromListCounting Instancer

--- MatchingGame/Assets/Scripts/Instancer.cs
+++ MatchingGame/Assets/Scripts/Instancer.cs
@@ -1,10 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Instancer : ScriptableObject
 {
     public GameObject prefab;
-    
+    private int num;
+
     public void CreateInstance()
     {
         Instantiate(prefab);
@@ -18,10 +20,19 @@ public class Instancer : ScriptableObject
 
     public void CreateInstanceFromList(Vector3DataList obj)
     {
-        for (var i = 0; i < obj.vector3DList.Count; i++)
+        foreach (var t in obj.vector3DList)
         {
-            Instantiate(prefab, obj.vector3DList[i].value, Quaternion.identity);   
+            Instantiate(prefab, t.value, Quaternion.identity);
         }
     }
     
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3DList.Count)
+        {
+            num = 0;
+        }
+    }
 }
